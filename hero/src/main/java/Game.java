b3c743f471b363609c0f70b82db8c9ff85d08f5e
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-02-12 09:27:40 +0000

    Function run is fully functional

--- hero/src/main/java/Game.java
+++ hero/src/main/java/Game.java
@@ -1,13 +1,20 @@
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.input.KeyStroke;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class Game {
+
     private Screen screen;
+    private int x = 10;
+    private int y = 10;
+
     public Game(){
         try {
             Terminal terminal = new DefaultTerminalFactory().createTerminal();
@@ -22,11 +29,34 @@ public class Game {
 
     private void draw() throws IOException{
         screen.clear();
-        screen.setCharacter(10, 10, new TextCharacter('X'));
+        screen.setCharacter(x, y, new TextCharacter('X'));
         screen.refresh();
     }
 
     public void run() throws IOException {
-        draw();
+        while (true){
+            draw();
+            KeyStroke key = screen.readInput();
+            processKey(key);
+        }
+    }
+
+    private void processKey(KeyStroke key){
+        switch (key.getKeyType()){
+            case ArrowDown:
+                y++;
+                break;
+            case ArrowLeft:
+                x--;
+                break;
+            case ArrowRight:
+                x++;
+                break;
+            case ArrowUp:
+                y--;
+                break;
+            default:
+                break;
+        }
     }
 }
